4f18cde19af6cc10e6e4f87f8b1577be7ab754b7
ezecodes <>
2024-03-30 14:32:32 +0100

    include the control buttons

--- components/InitialScreensComponents.js
+++ components/InitialScreensComponents.js
@@ -1,7 +1,6 @@
 import { useStore } from '../hooks/useStore';
 import colors from '../constants/colors';
-import { Entypo } from '@expo/vector-icons';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { Octicons } from '@expo/vector-icons';
 
 const Dots = ({}) =>  {
@@ -25,27 +24,64 @@ const Dots = ({}) =>  {
   )
 }
 
-const WelcomeTexts = ({header, body}) =>
-  <View style={{flex: 3, width: '90%'}}>
-    <Text style={{
-      fontSize: 23,
-      fontWeight: 'bold',
-      color: colors.grey_40,
-      textAlign: 'center'
-    }}>
-      {header}
-    </Text>
+const WelcomeTexts = ({header, body}) => {
+  
+
+  return (
+    <View style={{flex: 3, width: '90%'}}>
+      <Text style={{
+        fontSize: 23,
+        fontWeight: 'bold',
+        color: colors.grey_40,
+        textAlign: 'center',
+        fontFamily: 'Inter-SemiBold'
+      }}>
+        {header}
+      </Text>
     <Text
       style={{
         paddingTop: 10,
         fontSize: 15,
         color: colors.grey_30,
+        fontWeight: '100',
+        fontFamily: 'Inter-Regular',
         textAlign: "center"
       }}
     >
       {body}
     </Text>
   </View>
+  )
+}
 
 
-export {Dots, WelcomeTexts}
+const ControlButtons = () => {
+
+  return (
+    <View style={{flexDirection: 'row', paddingBottom: 15, justifyContent: 'space-between'}}>
+      
+      <Text style={styles.skipButton}>Skip</Text>
+      <Text style={styles.nextButton}>Next</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  skipButton: {
+    color: colors.grey_30
+  },
+  nextButton: {
+    backgroundColor: colors.blue_60,
+    color: '#fff',
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingRight: 20,
+    paddingLeft: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  }
+})
+  
+
+
+export {Dots, WelcomeTexts, ControlButtons}
